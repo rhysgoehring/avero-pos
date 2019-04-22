9feff6eacb2e3abb97c02ae2ee3ae9e1b8d64a9c
rhysgoehring <rhysgoehring@gmail.com>
2019-04-22 15:22:18 -0600

    installed styled-components, created global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
 }
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    font-family: Lato;
+  }
+
+  body {
+    background-color: orange;
+  }
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default App;
